1ff55335204234f3bed51e7e415f844996542796
f0rget <f0rget@pm.me>
2021-04-25 19:19:53 +0300

    poly and classes

---
+++ part2_base/Printable.hs
@@ -0,0 +1,12 @@
+class Printable a where
+    toString :: a -> String
+
+instance Printable Bool where
+    toString False = "false"
+    toString True  = "true"
+
+instance Printable () where
+    toString () = "unit type"
+
+instance (Printable a, Printable b) => Printable (a,b) where
+    toString (a, b) = "(" ++ toString a ++ "," ++ toString b ++ ")"
--- part2_base/poly_by_args.hs
+++ part2_base/poly_by_args.hs
@@ -17,3 +17,10 @@ h = snd
 -- it's not intended to be lambda-task
 on3 :: (b -> b -> b -> c) -> (a -> b) -> a -> a -> a -> c
 on3 op f x y z = op (f x) (f y) (f z)
+
+doItYourself = f . g . h where
+    h = max 42
+    g = (^3)
+    f = logBase 2
+
+swap = uncurry (flip (,))
